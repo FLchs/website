08a0028f82083de40b8c6338a7a30e5a31b68e8a
François Lachèse <francois.lachese@gmail.com>
2025-08-01 14:11:46 +0200

    refactor(engine): add mdx, sitemap and rss support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
 // @ts-check
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://francoislachese.com",
+  integrations: [mdx(), sitemap()],
   vite: { plugins: [tailwindcss()] },
+  markdown: {
+    shikiConfig: {
+      theme: "catppuccin-frappe", // to fix to macchiato
+    },
+  },
 });
